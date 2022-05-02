636400cd84aa3aab5d484c15fca0ea4e9db89c39
ShiQChen <huang2020yue@gmail.com>
2022-05-02 22:33:27 +0800

    update

---
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/exception/GuliMallExceptionControllerAdvice.java
@@ -0,0 +1,10 @@
+package com.atguigu.gulimall.product.exception;
+
+/**
+ * Created by 石其琛
+ * Project_Name gulimall
+ * Description
+ * On 2022/5/2 17:52
+ */
+public class GuliMallExceptionControllerAdvice {
+}
